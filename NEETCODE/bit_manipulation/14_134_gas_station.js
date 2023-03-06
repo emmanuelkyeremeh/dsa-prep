7aeda050ff5fb22c51b5c5b6e949ea7cdb966767
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2023-03-06 23:40:42 +0000

    added new soltuions

---
+++ NEETCODE/bit_manipulation/14_134_gas_station.js
@@ -0,0 +1,23 @@
+var canCompleteCircuit = function(gas, cost) {
+    let deficit = 0; //this will record the deficit in fuel whenever we encounter an index where it is impossible to visit the next index to prevent duplicate computation.
+
+    let start = 0; //record the index we are currently starting at. this will stay the same until it is impossible to visit the next index due to a negative value from the computation
+
+    let cal = 0 //calculated value of the available gas minus the cost at the particular point
+
+    for(let i=0;i<gas.length;i++){
+        cal += gas[i] - cost[i] //the amount of gas available must exceed or be the same as the distance hence we subtract the distance at i from the gas
+
+        if(cal<0){
+            start = i+ 1
+            deficit += cal //the amount of fuel required or the negative value recieved from the computation.we are setting this to the deficit so that we do not have to recompute already calculated paths
+            cal = 0 //set this to zero so we can start the calculation of the new paths uncharted yet
+        }
+    }
+    if(cal+deficit >= 0){
+        //return the last starting position if the excess fuel exceeds our deficit other wise there exists no solution
+        return start
+    }else{
+        return -1
+    }
+};
